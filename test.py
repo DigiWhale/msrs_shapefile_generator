c8d9361a0f8105dea60852aa330638b9b13b9df4
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-18 21:47:57 -0400

    1

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from leuvenmapmatching.matcher.distance import DistanceMatcher
 from leuvenmapmatching.map.inmem import InMemMap
 from leuvenmapmatching import visualization as mmviz
+import pandas as pd
 
 # map_con = InMemMap("mymap", graph={
 #     "A": ((1, 1), ["B", "C", "X"]),
@@ -14,12 +15,14 @@ from leuvenmapmatching import visualization as mmviz
 #     "K": ((1, 5), ["B", "D", "L"]),
 #     "L": ((2, 6), ["K", "D", "F"])
 # }, use_latlon=False)
+df = pd.read('master_log.csv')
+
 map_con = InMemMap("../maryland-latest.osm.pbf", use_latlon=True)
 
-path = [(0.8, 0.7), (0.9, 0.7), (1.1, 1.0), (1.2, 1.5), (1.2, 1.6), (1.1, 2.0),
-        (1.1, 2.3), (1.3, 2.9), (1.2, 3.1), (1.5, 3.2), (1.8, 3.5), (2.0, 3.7),
-        (2.3, 3.5), (2.4, 3.2), (2.6, 3.1), (2.9, 3.1), (3.0, 3.2),
-        (3.1, 3.8), (3.0, 4.0), (3.1, 4.3), (3.1, 4.6), (3.0, 4.9)]
+path = []
+
+for index, row in df.iterrows():
+    path.append((df['jetson_rpi_lat'].iloc[index], df['jetson_rpi_lng'].iloc[index]))
 
 matcher = DistanceMatcher(map_con, max_dist=2, obs_noise=1, min_prob_norm=0.5, max_lattice_width=5)
 states, _ = matcher.match(path)
