fbc196038ee7fd05995404b0dc10f097614edf68
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-18 21:44:30 -0400

    1

--- test.py
+++ test.py
@@ -2,18 +2,19 @@ from leuvenmapmatching.matcher.distance import DistanceMatcher
 from leuvenmapmatching.map.inmem import InMemMap
 from leuvenmapmatching import visualization as mmviz
 
-map_con = InMemMap("mymap", graph={
-    "A": ((1, 1), ["B", "C", "X"]),
-    "B": ((1, 3), ["A", "C", "D", "K"]),
-    "C": ((2, 2), ["A", "B", "D", "E", "X", "Y"]),
-    "D": ((2, 4), ["B", "C", "F", "E", "K", "L"]),
-    "E": ((3, 3), ["C", "D", "F", "Y"]),
-    "F": ((3, 5), ["D", "E", "L"]),
-    "X": ((2, 0), ["A", "C", "Y"]),
-    "Y": ((3, 1), ["X", "C", "E"]),
-    "K": ((1, 5), ["B", "D", "L"]),
-    "L": ((2, 6), ["K", "D", "F"])
-}, use_latlon=False)
+# map_con = InMemMap("mymap", graph={
+#     "A": ((1, 1), ["B", "C", "X"]),
+#     "B": ((1, 3), ["A", "C", "D", "K"]),
+#     "C": ((2, 2), ["A", "B", "D", "E", "X", "Y"]),
+#     "D": ((2, 4), ["B", "C", "F", "E", "K", "L"]),
+#     "E": ((3, 3), ["C", "D", "F", "Y"]),
+#     "F": ((3, 5), ["D", "E", "L"]),
+#     "X": ((2, 0), ["A", "C", "Y"]),
+#     "Y": ((3, 1), ["X", "C", "E"]),
+#     "K": ((1, 5), ["B", "D", "L"]),
+#     "L": ((2, 6), ["K", "D", "F"])
+# }, use_latlon=False)
+map_con = InMemMap("../maryland-latest.osm.pbf", use_latlon=True)
 
 path = [(0.8, 0.7), (0.9, 0.7), (1.1, 1.0), (1.2, 1.5), (1.2, 1.6), (1.1, 2.0),
         (1.1, 2.3), (1.3, 2.9), (1.2, 3.1), (1.5, 3.2), (1.8, 3.5), (2.0, 3.7),
