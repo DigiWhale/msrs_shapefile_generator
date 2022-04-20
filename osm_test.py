cc21a7f14dce982ab904650c955eb7768bdea740
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-19 22:29:33 -0400

    1

--- osm_test.py
+++ osm_test.py
@@ -1,3 +1,4 @@
 import osmnx as ox
 G = ox.graph_from_bbox(37.79, 37.78, -122.41, -122.43, network_type='drive')
-ox.plot_graph(ox.project_graph(G), filename = "plot.png", show = False, save = True, close = True)
+fig, ax = ox.plot_graph(ox.project_graph(G))
+fig.savefig('maryland.png')
