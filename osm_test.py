7c2becc17d9d5852e247f6880b3648b008db2b12
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-19 22:25:03 -0400

    1

--- osm_test.py
+++ osm_test.py
@@ -1,3 +1,3 @@
 import osmnx as ox
 G = ox.graph_from_bbox(37.79, 37.78, -122.41, -122.43, network_type='drive')
-ox.save_graphml(G, 'maryland.graphml')
+ox.plot_graph(G, save=True, file_format='svg')
